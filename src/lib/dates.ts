e8ab1c3c8ab5d2eb08ff0dcabe0e5ee9afd3b1bc
Alamgir Ahamed <alamgir72435@gmail.com>
2023-03-10 16:54:59 +0600

    Updates

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 // Date String Like (3 Feb, 2023)
-export function DateStringMonthYear(date:Date):string {
+export function DateStringMonthYear(date: Date): string {
   if (date === null || date === undefined) return '';
   const format = String(moment(date).format('ll')).split(',');
   const _ = format[0].split(' ');
@@ -10,7 +10,7 @@ export function DateStringMonthYear(date:Date):string {
 
 
 // Basic Date Format like (03/02/2023)
-export function basicFormat(date:Date):string {
+export function basicFormat(date: Date): string {
   if (date === null || date === undefined) return '';
   const _ = String(moment(date).format('L')).split('/');
   return `${_[1]}/${_[0]}/${_[2]}`;
@@ -18,7 +18,7 @@ export function basicFormat(date:Date):string {
 
 
 // date range build for mongodb date range query
-export function dateRangeBuild(startDate:number, endDate:number):object{
+export function dateRangeBuild(startDate: number, endDate: number): object {
   const _d_start = new Date(startDate)
     .toLocaleDateString(`fr-CA`)
     .split("/")
@@ -33,3 +33,19 @@ export function dateRangeBuild(startDate:number, endDate:number):object{
     end: new Date(`${_d_end}T23:59:59.999Z`),
   };
 }
+
+
+
+export function DateRangeToArray(start:Date, end:Date){
+  return new Promise((resolve, reject) => {
+   let dates = [];
+   // loop two dates
+   var a = moment(start);
+   var b = moment(end);
+   for (var m = moment(a); m.isBefore(b); m.add(1, 'days')) {
+       dates.push(String(m.format('L')));
+   }
+   resolve({ dates })
+  })
+ }
+
